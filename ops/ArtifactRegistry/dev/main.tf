3d6420b25c9271d63e9b5e899900cfdc7a341384
ajaymadamshetty <125345486+ajaymadamshetty@users.noreply.github.com>
2024-01-17 20:08:23 +0530

    Update main.tf

--- ops/ArtifactRegistry/dev/main.tf
+++ ops/ArtifactRegistry/dev/main.tf
@@ -15,7 +15,7 @@ terraform {
   }
 }
 provider "google" {
-  project = "	ajayjenkins"
+  project = "ajayjenkins"
 }
 resource "google_artifact_registry_repository" "my-repo" {
   location      = var.location
