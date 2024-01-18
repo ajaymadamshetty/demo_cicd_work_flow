79eee12a82fc6c1b3e0596e8a4fd3905bc216efe
ajaymadamshetty <125345486+ajaymadamshetty@users.noreply.github.com>
2024-01-18 15:29:04 +0530

    Update main.tf

--- ops/CloudRunService/dev/main.tf
+++ ops/CloudRunService/dev/main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 terraform {
   backend "gcs" {
-    bucket = "anil-terraform-statefiles" # GCS bucket name to store terraform tfstate
+    bucket = "anil" # GCS bucket name to store terraform tfstate
     prefix = "cicd-demo/dev/CloudRunService"               # Prefix name should be unique for each Terraform project having same remote state bucket.
   }
 }
